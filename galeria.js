cbd52ae52236d145e08c6fd72546b7606078fdac
rsnd333 <167139732+rsnd333@users.noreply.github.com>
2025-05-30 19:47:29 -0300

    Add files via upload

---
+++ galeria.js
@@ -0,0 +1,6 @@
+const ano = document.querySelector("#ano-atual");
+ano.innerHTML = new Date().getFullYear();
+
+ScrollReveal().reveal('.section-um', { delay: 300 });
+ScrollReveal().reveal('.section-dois', { delay: 300 });
+ScrollReveal().reveal('.section-tres', { delay: 300 });
